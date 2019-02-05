8ca9f4c62ba6f1d38b51128ee94dd376f1d3d0b6
Niklas Gustafsson <niklas.m.gustafsson@gmail.com>
2019-02-05 23:42:41 +0100

    fixed the option dist: -30

--- public/script/cardListener.js
+++ public/script/cardListener.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    $('.carousel').carousel();
+    $('.carousel').carousel({
+      dist:-30
+    });
+    
   });
+
+
+ /*  $(document).ready(function(){
+    $('.carousel').carousel(
+    {
+      dist: 0,
+      padding: 0,
+      fullWidth: true,
+      indicators: true,
+      duration: 100,
+    }
+    );
+  }); */
