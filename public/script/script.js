158f0d6dd9bdc1714c0a40895dae41842e8a745c
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2019-02-19 23:42:56 +0100

    Updated owl version

--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
 	$('.owl-carousel').owlCarousel({
-    loop: false,
-    items: 3,
-    margin: 100,
-    responsive: {
-        600: {
-            items: 4
-        }
-    }
+        loop: false,
+        items: 3,
+        margin: 10
 	});
 });
