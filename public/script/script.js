1f778dd40c52773c7b4e49c3783e17c73432c9e7
Niklas Gustafsson <niklas.m.gustafsson@gmail.com>
2019-02-24 00:06:29 +0100

    soon to be done! Need to fix the size of the carousel

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$('.owl-carousel').owlCarousel({
         loop: false,
-        items: 3,
-        margin: 10
-	});
+        items: 4,
+        margin: 10,
+        center: true,
+        startPosition: 1,
+        autoHeight:true
+
+    });
+
+
+
 });
